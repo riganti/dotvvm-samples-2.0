a3005cbef3670e22fdb9ad68fa95d4531c744c50
Maroš Janota <maros.janota@riganti.cz>
2018-05-25 15:03:18 +0200

    OtherFeaters tempalte added

--- AnnouncingDotvvm/AnnouncingDotvvm.OtherFeatures/DotvvmStartup.cs
+++ AnnouncingDotvvm/AnnouncingDotvvm.OtherFeatures/DotvvmStartup.cs
@@ -25,7 +25,6 @@ namespace AnnouncingDotvvm.OtherFeatures
         {
             config.Markup.AddMarkupControl("cc", "CollectionSample", "Controls/CollectionSample.dotcontrol");
             config.Markup.AddMarkupControl("cc", "FormControlsSample", "embedded://AnnouncingDotvvm.EmbeddedControls/Controls.FormControlsSample.dotcontrol");
-            config.Markup.AddMarkupControl("cc", "FooterTemplate", "embedded://AnnouncingDotvvm.EmbeddedControls/Controls.FooterTemplate.dotcontrol");
             // register code-only controls and markup controls
         }
 
